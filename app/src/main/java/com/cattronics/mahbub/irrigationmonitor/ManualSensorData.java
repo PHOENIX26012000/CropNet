108913cf6375f81e711826fce5c6ee0854d97260
Mahbubur Rahman <mahbubcu14@gmail.com>
2018-05-13 02:34:20 +0600

    layout changed. Don't copy this version. This is corrupted because Java code have errors.

---
+++ app/src/main/java/com/cattronics/mahbub/irrigationmonitor/ManualSensorData.java
@@ -0,0 +1,13 @@
+package com.cattronics.mahbub.irrigationmonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManualSensorData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_sensor_data);
+    }
+}
